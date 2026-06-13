9892d3db7554b4f8c54250918abb43d29339a375
almudena vivanco <almudena.vivanco@mail.schwarz>
2026-06-13 19:01:15 +0200

    added SLOS

--- k6/basic-load.js
+++ k6/basic-load.js
@@ -5,16 +5,21 @@ import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporte
 export const options = {
   vus: 1,
   duration: '30s',
+  thresholds: {
+    'http_req_duration{name:home}': ['p(95)<500'],
+    'http_req_duration{name:productpage}': ['p(95)<700'],
+    'http_req_failed': ['rate<0.01'],
+  },
 };
 
 export default function () {
-  const res1 = http.get('http://localhost:9080/');
+  const res1 = http.get('http://localhost:9080/', { tags: { name: 'home' } });
   check(res1, {
     'home page status is 200': (r) => r.status === 200,
   });
   sleep(1);
 
-  const res2 = http.get('http://localhost:9080/productpage?u=normal');
+  const res2 = http.get('http://localhost:9080/productpage?u=normal', { tags: { name: 'productpage' } });
   check(res2, {
     'productpage status is 200': (r) => r.status === 200,
   });
